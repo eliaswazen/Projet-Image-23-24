bf0e024d2ed123a889c6d5a61b75c947fd51b927
Cubevayster <arthurvillaroya@gmail.com>
2023-11-12 17:31:14 +0100

    Rapport

--- Projet/All.py
+++ Projet/All.py
@@ -20,7 +20,7 @@ cascade_pieton_src = 'XML/pedestrian.xml'
 cascade_bus_src = 'XML/Bus_front.xml'
 cascade_twoWhell_src = 'XML/two_wheeler.xml'
 
-video_src = 'Videos/velo.mp4'
+video_src = 'Videos/pedestrians.avi'
 
 cap = cv2.VideoCapture(video_src)
 
@@ -36,15 +36,15 @@ while True:
         break
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    cars = car_cascade.detectMultiScale(gray, 1.1, 2)
-    pedestrians = pieton_cascade.detectMultiScale(gray, 1.1, 2)
-    bus = bus_cascade.detectMultiScale(gray, 1.1, 2)
-    two_whell = twoWhell_cascade.detectMultiScale(gray, 1.1, 2)
+    cars = car_cascade.detectMultiScale(gray, 1.2, 2)
+    pedestrians = pieton_cascade.detectMultiScale(gray, 1.2, 1)
+    bus = bus_cascade.detectMultiScale(gray, 1.16, 1)
+    two_whell = twoWhell_cascade.detectMultiScale(gray, 1.01, 1)
 
-    #for (x, y, w, h) in bus:
-    #    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    #    label = f"Bus"  # Create the label
-    #    cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    for (x, y, w, h) in bus:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        label = f"Bus"  # Create the label
+        cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
     for (x, y, w, h) in two_whell:
         cv2.rectangle(img, (x, y), (x + w, y + h), (210, 255, 0), 2)
@@ -52,14 +52,14 @@ while True:
         cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (210, 255, 0), 2)
 
     #for (x, y, w, h) in cars:
-    #   cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
-    #  label = f"Voiture"  # Create the label
+    #    cv2.rectangle(img, (x, y), (x + w, y + h), (255,0,0), 2)
+    #    label = f"Voiture"  # Create the label
     #    cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
 
-    #for (x, y, w, h) in pedestrians:
-    #    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
-    #    label = f"Pieton"  # Create the label
-    #    cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 210), 2)
+    for (x, y, w, h) in pedestrians:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        label = f"Pieton"  # Create the label
+        cv2.putText(img, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 210), 2)
 
     cv2.imshow('video', img)
 
